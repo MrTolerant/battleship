1885d41f13a4660801aae8ad2022328930e3a75c
mrtolerant <lebedevpetr@outlook.com>
2020-08-19 12:44:07 +0300

    generate simple

--- src/Battleship.js
+++ src/Battleship.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 import Board from './Board'
 // import initialGrid from './initialGame.js'
@@ -8,32 +8,81 @@ function getRandomInt(max) {
 }
 
 const BoardContainer = () => {
+  const [ships, setShips] = useState([])
+
+  const addShips = (count = 1) => {
+    let newShips = []
+    for (let i = 1; i < count; i += 1) {
+      const startPos = { x: getRandomInt(gridSize), y: getRandomInt(gridSize) }
+      const length = getRandomInt(3) + 2
+      const direction = getRandomInt(20) ? 'vertical' : 'horizontal'
+      newShips = [...newShips, { startPos, length, direction }]
+      console.log('new ships', newShips)
+    }
+    setShips([...newShips])
+    console.log('add ship')
+  }
+
   const randomGrid = (gridSize, shipsCount) => {
     let grid = []
-    const ships = new Array(shipsCount).fill(null).map((ship) => `${getRandomInt(gridSize)}-${getRandomInt(gridSize)}`)
     for (let x = 0; x < gridSize; x += 1) {
       grid = [...grid, []]
       for (let y = 0; y < gridSize; y += 1) {
-        grid[x] = [...grid[x], { ship: ships.includes(`${x}-${y}`) ? 1 : 0, hit: 0 }]
+        grid[x] = [...grid[x], { ship: 0, hit: 0, inactive: false }]
       }
     }
-    console.log('ships', ships)
-    console.log('grid', grid)
+
     return grid
   }
+
+  useEffect(() => {
+    addShips(shipsCount)
+  }, [])
+
+  useEffect(() => {
+    putShips()
+  }, [ships])
 
   const gridSize = 10
   const shipsCount = 10 // grid.flat().reduce((acc, rec) => (rec.ship ? (acc += 1) : acc), 0)
   const [grid, setGrid] = useState(randomGrid(gridSize, shipsCount))
   const maxMiss = 5
   const initialState = {
-    shots: shipsCount + maxMiss,
+    shots: ships.length + maxMiss,
     hits: 0
   }
   const [shots, setShots] = useState(initialState.shots)
   const [hits, setHits] = useState(initialState.hits)
 
-  const addShip = () => {}
+  const putShips = () => {
+    let newGrid = grid
+    ships.forEach((ship) => {
+      const { x, y } = ship.startPos
+      if (ship.direction === 'vertical') {
+        if (y + ship.length <= gridSize) {
+          for (let i = y; i < y + ship.length; i += 1) {
+            newGrid[x][i].ship = 1
+          }
+        } else {
+          for (let i = y; i > y - ship.length; i -= 1) {
+            newGrid[x][i].ship = 1
+          }
+        }
+      } else {
+        if (x + ship.length <= gridSize) {
+          for (let i = x; i < x + ship.length; i += 1) {
+            newGrid[x][i].ship = 1
+          }
+        } else {
+          for (let i = x; i > x - ship.length; i -= 1) {
+            newGrid[x][i].ship = 1
+          }
+        }
+      }
+    })
+    setGrid([...newGrid])
+    console.log('new ships', grid)
+  }
 
   const onClick = (x, y) => {
     if (!grid[x][y].hit) {
@@ -47,7 +96,9 @@ const BoardContainer = () => {
     setShots(initialState.shots)
     setHits(initialState.hits)
     setGrid(randomGrid(gridSize, shipsCount))
+    addShips(shipsCount)
   }
+
   return (
     <div className="flex flex-col">
       <div className="text-2xl flex flex-row w-full">
@@ -74,9 +125,9 @@ const BoardContainer = () => {
       </div>
       {hits < shipsCount && shots > 0 && (
         <>
-          <button className="transition duration-500 ease-in-out m-2 bg-yellow-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" onClick={addShip}>
+          {/* <button className="transition duration-500 ease-in-out m-2 bg-yellow-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" onClick={addShip}>
             AddShip
-          </button>
+          </button> */}
           <button className="transition duration-500 ease-in-out m-2 bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={restart}>
             restart
           </button>
